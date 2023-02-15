c2b6f671d9d8f64250181e571966ed9eb36b4446
Sumit <gurjar.sumitu4500@.com>
2023-02-15 07:24:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+import CheckoutForm from "./CheckoutForm";
+import "./App.css";
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe(
+  "pk_test_51Ma1ZoSI8hIW8wNaMke2EAbE7QxdY5ggZdOgPeiA9hU2hgowIjHyOYUfug7tzc5fyr9UmQyhnpFlu9s2mptj8ytN00tMD2XOBp"
+);
+
+export default function App() {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data==========>>>", data);
+        setClientSecret(data.clientSecret);
+      });
+  }, []);
+
+  const appearance = {
+    theme: "night",
+    variables: {
+      colorPrimary: "#0570de",
+      colorBackground: "#ffffff",
+      colorText: "#ffffff",
+      colorDanger: "#df1b41",
+      fontFamily: "Ideal Sans, system-ui, sans-serif",
+      spacingUnit: "2px",
+      borderRadius: "4px",
+      // See all possible variables below
+    },
+    rules: {
+      ".Input": {
+        backgroundColor: "var(--colorDanger)",
+      },
+    },
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
